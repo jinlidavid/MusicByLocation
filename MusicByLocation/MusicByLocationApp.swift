dd5a8575aeb67a1a819b2640c988a4bd5ffb9ab1
davidjinli <61616862+davidjinli@users.noreply.github.com>
2022-02-28 19:12:15 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by David Jin Li on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
